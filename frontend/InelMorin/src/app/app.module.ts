bf4ba536a82f0b12d8ce18e3dac4105a2783b68c
SeifeddineJaziri <seifeddinejaziri123@gmail.com>
2024-02-13 13:41:30 +0100

    I have added the ngModel variables

--- frontend/InelMorin/src/app/app.module.ts
+++ frontend/InelMorin/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AcceuilComponent } from './acceuil/acceuil.component';
@@ -22,7 +22,8 @@ import { EnseignantsComponent } from './enseignants/enseignants.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
