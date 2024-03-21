62e2e274d8a86f7ca9c855a53bf87a5d68cc13ff
KoussayDhifi <koussaydhifi48@gmail.com>
2024-03-21 18:15:25 +0100

    Fix navBar Bug

--- frontend/InelMorin/src/app/nav-bar/nav-bar.component.ts
+++ frontend/InelMorin/src/app/nav-bar/nav-bar.component.ts
@@ -10,6 +10,7 @@ export class NavBarComponent {
 
   public display1:string = "display:none";
   public display2:string ="display:none";
+  public display3:string ="display:none";
 
   toggleItems () {
     (this.display == "display:none")? this.display="display:block;" : this.display="display:none";
@@ -23,8 +24,8 @@ export class NavBarComponent {
     (this.display2 == "display:none") ? this.display2 ="display:block;" : this.display2="display:none";
   }
 
-  toggleItems2() {
-    (this.display2 == "display:none")? this.display2="display:block;" : this.display2="display:none";
+  toggleItems3() {
+    (this.display3 == "display:none")? this.display3="display:block;" : this.display3="display:none";
   }
 
 }
