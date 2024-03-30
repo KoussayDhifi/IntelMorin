0a4267fbcbc8dfaf450751c8a9857debb457c425
KoussayDhifi <koussaydhifi48@gmail.com>
2024-03-30 17:22:28 +0100

    Finish paiment Tutor (frontend)

--- frontend/InelMorin/src/app/tutor-payment/tutor-payment.component.ts
+++ frontend/InelMorin/src/app/tutor-payment/tutor-payment.component.ts
@@ -8,13 +8,23 @@ import { Component } from '@angular/core';
 export class TutorPaymentComponent {
   public profName:any = "";
   public dateP:any = "";
-
+  public expand:boolean | undefined;
+  
   isDateFormatValid(date: string): boolean {
     const dateFormatRegex = /(0[1-9]|1[012])\/[0-9]{4}/;
     console.log(dateFormatRegex.test(date))
     return dateFormatRegex.test(date);
   }
+  
 
+  toggleGrid () {
+    if (this.expand == undefined) {
+      this.expand = false;
+    }
+    this.expand = !this.expand;
+
+    
+  }
 
   submitTest() {
     console.log("Submitted yo");
