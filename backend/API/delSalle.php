58948ccadc18a4506ffe15d6cf5ec2ab674b178f
KoussayDhifi <koussaydhifi48@gmail.com>
2024-03-31 14:11:07 +0100

    Add CRUD for classrooms

---
+++ backend/API/delSalle.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once '../connect/db_connect.php';
+
+$data = json_decode(file_get_contents('php://input'),true);
+
+$labelSalle = $data['salle'];
+
+$query = "DELETE FROM CLASSROOM WHERE LABELCLASS='$labelSalle'";
+
+
+if ($conn->query($query) == TRUE) {
+    http_response_code(200);
+    echo json_encode(array('msg'=>'Supprimer avec succes !!'));
+}else {
+    http_response_code(400);
+    echo json_encode(array('msg'=>'Repeter une autre fois'));
+}
